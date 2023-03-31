b1a024c1f64fab89bead0c43dc23016d92abfb35
Shivang1102 <shivanggupta1102@gmail.com>
2023-03-31 16:09:07 +0530

    Header

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,17 @@
+import meals from'../../assests/meals.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header=(props)=>{
+    return <>
+        <header  className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton/>
+        </header>
+      <div  className={classes['main-image']}>
+        <img  src={meals}  alt='A table full of delicious food'/>
+      </div>
+    </>
+}
+
+ export default Header
